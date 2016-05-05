c6ea7adf9aee8e8c5d9b1ecbc3eb35925db413f7
Zeph Gardler <zgardler@gmail.com>
2016-05-05 04:28:19 +0000

    now use port 80 not 8000

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 docker stop halostats
 docker rm halostats
 docker build -t zgardler/halostats:test .
-docker run -v $(pwd):/app -d --name halostats -p 8000:5000 zgardler/halostats:test
+docker run -v $(pwd):/app -d --name halostats -p 80:5000 zgardler/halostats:test
 
 sleep 2
-curl localhost:8000
+curl localhost
 
 
